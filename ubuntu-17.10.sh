0e06c5cd4249f09dfdd2e211414c56597337b776
Rusty Green <rustymgreen@gmail.com>
2018-02-26 14:53:45 -0500

    Added node and yarn installations

--- ubuntu-17.10.sh
+++ ubuntu-17.10.sh
@@ -59,9 +59,12 @@ sudo apt-get update
 sudo apt-get install pantheon-terminal
 
 # NPM
-sudo apt install npm
+sudo apt update
+sudo apt install nodejs npm
 
 # Yarn
+sudo apt update
+sudo apt remove cmdtest
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install yarn
